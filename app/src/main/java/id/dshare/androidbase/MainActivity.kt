503f6c709d48055a2ad0aec33c297a06e987481a
Agung Kurniawan <agung.k@gdn-commerce.com>
2020-11-16 05:27:55 +0700

    added Viewbinding

--- app/src/main/java/id/dshare/androidbase/MainActivity.kt
+++ app/src/main/java/id/dshare/androidbase/MainActivity.kt
@@ -2,10 +2,16 @@ package id.dshare.androidbase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import id.dshare.androidbase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+  private lateinit var mBinding: ActivityMainBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    mBinding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(mBinding.root)
+
+    mBinding.tvHello.text = "Halo dunia!"
   }
 }
